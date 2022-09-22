d246e23f6f96ac6bb40ce9bdc4e2540380646f19
daring02 <1482882482@qq.com>
2022-09-22 21:46:04 +0800

    顺序消息发送

---
+++ src/main/java/com/dar/rocketmq/producers/sequence/Producer.java
@@ -0,0 +1,49 @@
+package com.dar.rocketmq.producers.sequence;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+/**
+ * @author :wx
+ * @description :
+ * @create :2022-09-22 21:30:00
+ */
+public class Producer {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        try {
+            DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+            producer.start();
+            Thread.sleep(3000);//等待producer启动完成
+            String[] tags = new String[] {"TagA", "TagB", "TagC", "TagD", "TagE"};
+            for (int i = 0; i < 50; i++) {
+                int orderId = i % 10;
+                Message msg =
+                        new Message("TopicTest", tags[i % tags.length], "KEY" + i,
+                                ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                    @Override
+                    public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                        Integer id = (Integer) arg;
+                        int index = id % mqs.size();
+                        return mqs.get(index);
+                    }
+                }, orderId);
+                System.out.printf("%s%n", sendResult);
+            }
+
+            producer.shutdown();
+        } catch (MQClientException | RemotingException | MQBrokerException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
